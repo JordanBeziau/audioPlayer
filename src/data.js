fd0470510f351a968dffb42b04aef51dab5be9f9
unknown <jordanbeziau@gmail.com>
2017-04-05 17:04:42 +0200

    Ajout fonctionnalités génériques du player

---
+++ src/data.js
@@ -0,0 +1,16 @@
+// Stockage des musiques
+let tabMusiques = [];
+tabMusiques[0] = {
+  titre: "Money money",
+  artiste: "artiste 1",
+  genre: "Dance/Soul",
+  cover: "cover-1.jpg",
+  fichier: "MONEY MONEY.mp3"
+}
+tabMusiques[1] = {
+  titre: "Touch Di Road",
+  artiste: "artiste 2",
+  genre: "Dance/Soul",
+  cover: "cover-2.jpg",
+  fichier: "R.E.C (RED EYE CREW) -TOUCH DI ROAD - OVERLOAD RIDDIM.mp3"
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,94 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', () => {
 
 	// Ciblages
-	const player = document.getElementById('player');
-	const btn_pp = document.getElementById('pp');
-	let btn_stop = document.querySelector('.btn-stop');
-	let btn_prev = document.querySelector('.btn-prev');
-	let btn_next = document.querySelector('.btn-next');
+	const player = document.getElementById('player'),
+	      btnPP = document.getElementById('pp'),
+	      btnStop = document.querySelector('.btn-stop'),
+	      btnPrev = document.querySelector('.btn-prev'),
+	      btnNext = document.querySelector('.btn-next'),
+        affTitre = document.querySelector('h3'),
+        affArtiste = document.querySelector('h4'),
+        affCover = document.querySelector('.img-song img'),
+        affListing = document.querySelector('#listing select');
+  // Initialisation
+  const nbrMusiques = tabMusiques.length;
+  let listing = '';
+  let numMusique = 0,
+      progressBar;
+
+  // Init lecteur musique 1
+  affTitre.textContent = tabMusiques[0].titre;
+  affArtiste.textContent = tabMusiques[0].artiste;
+  affCover.src = `cover/${tabMusiques[0].cover}`;
+  player.src = `mp3/${tabMusiques[0].fichier}`;
+
+  // Liste tous les titres
+  for (let i = 0; i < nbrMusiques; i++) {
+    listing += `<option value="${i}">${ tabMusiques[i].titre }</option>`;
+  }
+  affListing.innerHTML = listing;
+
+  // Gestion du listing
+  affListing.addEventListener('change', () => {
+    numMusique = affListing.value;
+    setActualAudio();
+    playAudio();
+  });
 
 	// Gestion du BTN PLAY
-	btn_pp.addEventListener('click', function() {
+	btnPP.addEventListener('click', () => {
 		if (player.paused===true) {
-			player.play();
-			btn_pp.className = "btn-pause";
+      playAudio();
 		} else {
 			player.pause();
-			btn_pp.className = "btn-play";
+			btnPP.className = "btn-play";
 		}
 	});
 
 	// Gestion du BTN STOP
-	btn_stop.addEventListener('click', function() {
+	btnStop.addEventListener('click', () => {
 		player.pause();
 		player.currentTime = 0;
-		btn_pp.className = "btn-play";
+		btnPP.className = "btn-play";
 	});
 
+  // Gestion du BTN PREV
+  btnPrev.addEventListener('click', () => {
+    if (numMusique <= 0) {
+      numMusique = nbrMusiques - 1;
+    } else {
+      numMusique--;
+    }
+    setActualAudio();
+  });
+
+  // Gestion du BTN NEXT
+  btnNext.addEventListener('click', () => {
+    if (numMusique >= nbrMusiques - 1) {
+      numMusique = 0;
+    } else {
+      numMusique++;
+    }
+    setActualAudio();
+    playAudio();
+  });
+
+  // Barre de temps audio
+  player.addEventListener('timeupdate', () => {
+    progressBar = player.currentTime * 100 / player.duration;
+    console.log(progressBar);
+  });
+
+  // Fonction set actuel piste audio
+  const setActualAudio = () => {
+    affTitre.textContent = tabMusiques[numMusique].titre;
+    affArtiste.textContent = tabMusiques[numMusique].artiste;
+    affCover.src = `cover/${tabMusiques[numMusique].cover}`;
+    player.src = `mp3/${tabMusiques[numMusique].fichier}`;
+  }
+  // Fonction Play affiche btn pause
+  const playAudio = () => {
+    player.play();
+    btnPP.className = "btn-pause";
+  }
 });
